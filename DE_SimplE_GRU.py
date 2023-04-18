ac13336b693203a278a6628c3a3897a4d0a8fb2d
Diep Quyen Thang <thangdq@2nf.com.vn>
2023-04-18 13:37:39 +0700

    add init_embeddings func

--- DE_SimplE_GRU.py
+++ DE_SimplE_GRU.py
@@ -20,15 +20,15 @@ class DE_SimplE_GRU(nn.Module):
 
         self.ent_embs_h = nn.Embedding(dataset.numEnt(), params.s_emb_dim).cuda()
         self.ent_embs_t = nn.Embedding(dataset.numEnt(), params.s_emb_dim).cuda()
-        self.rel_emb_f = nn.Embedding(dataset.numRel(), params.s_emb_dim+params.t_emb_dim).cuda()
-        self.rel_emb_i = nn.Embedding(dataset.numRel(), params.s_emb_dim+params.t_emb_dim).cuda()
+        self.rel_embs_f = nn.Embedding(dataset.numRel(), params.s_emb_dim+params.t_emb_dim).cuda()
+        self.rel_embs_i = nn.Embedding(dataset.numRel(), params.s_emb_dim+params.t_emb_dim).cuda()
 
         self.ent_encoder_h = nn.GRU(params.s_emb_dim, self.hidden_size, batch_first=True).cuda()
         self.ent_encoder_t = nn.GRU(params.s_emb_dim, self.hidden_size, batch_first=True).cuda()
         self.rel_encoder_f = nn.GRU(params.s_emb_dim+params.t_emb_dim, self.hidden_size, batch_first=True).cuda()
         self.rel_encoder_i = nn.GRU(params.s_emb_dim+params.t_emb_dim, self.hidden_size, batch_first=True).cuda()
 
-        self.create_time_encode()
+        self.create_time_embedd()
 
         self.time_nl = torch.sin
 
@@ -36,8 +36,39 @@ class DE_SimplE_GRU(nn.Module):
         nn.init.xavier_uniform_(self.ent_embs_t.weight)
         nn.init.xavier_uniform_(self.rel_emb_f.weight)
         nn.init.xavier_uniform_(self.rel_emb_i.weight)
+    
+    def init_embeddings(self, pretrained_vocab = None, pretrained_emb = None):
+        ...
+        self.word_ent_embedd = {}
+        self.word_rel_embedd = {}
+        if pretrained_vocab is None or pretrained_vocab is None:
+            with open(Glove_path, encoding='utf-8') as glove:
+                for line in glove:
+                    word, vec = line.split(' ', 1)
+                    if word in self.dataset.ent2id:
+                        self.word_ent_embedd[self.dataset.ent2id[word]] = np.fromstring(vec, sep=' ')
+                    if word in self.dataset.rel2id:
+                        self.word_rel_embedd[self.dataset.rel2id[word]] = np.fromstring(vec, sep=' ')
 
-    def create_time_encode(self):
+        ent_uninitialized = [word for word in self.dataset.ent2id.values() if not word in self.word_ent_embedd]
+        rel_uninitialized = [word for word in self.dataset.rel2id.values() if not word in self.word_rel_embedd]
+
+        for word in ent_uninitialized:
+            self.word_ent_embedd[word] = np.random.normal(size=300)
+        
+        for word in rel_uninitialized:
+            self.word_rel_embedd[word] = np.random.normal(size=300)
+
+        self.ent_embedd_matrix = np.zeros(len(self.word_ent_embedd), 300)
+        self.rel_embedd_matrix = np.zeros(len(self.word_rel_embedd), 300)
+
+        for word in self.word_ent_embedd:
+            self.ent_embedd_matrix[word] = self.word_ent_embedd[word]
+        
+        for word in self.word_rel_embedd:
+            self.rel_embedd_matrix[word] = self.word_rel_embedd[word]
+
+    def create_time_embedd(self):
         # frequency embeddings for the entities
         self.m_freq_h = nn.Embedding(self.dataset.numEnt(), self.params.t_emb_dim).cuda()
         self.m_freq_t = nn.Embedding(self.dataset.numEnt(), self.params.t_emb_dim).cuda()
@@ -121,7 +152,8 @@ class DE_SimplE_GRU(nn.Module):
         r_s2, r_enc_s2 = self.rel_encoder_f(r_embs2)
         t_s2, t_enc_s2 = self.ent_encoder_t(t_embs2) 
 
-        return h_enc_s1, r_enc_s1, t_enc_s1, h_enc_s2, r_enc_s2, t_enc_s2
+        return h_s1, r_s1, t_s1, h_s2, r_s2, t_s2
+        # return h_enc_s1, r_enc_s1, t_enc_s1, h_enc_s2, r_enc_s2, t_enc_s2
     
     def forward(self, heads, rels, tails, years, months, days):
         h_enc_s1, r_enc_s1, t_enc_s1, h_enc_s2, r_enc_s2, t_enc_s2 = self.getEncoder(heads, rels, tails, years, months, days)
